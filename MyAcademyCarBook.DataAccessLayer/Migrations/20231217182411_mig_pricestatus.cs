e6ea010cf35f975ecdcebbe64939fdd8e83e89b2
Adem Yıldırım <yldrmadem52@gmail.com>
2023-12-17 23:30:03 +0300

    First Commit

---
+++ MyAcademyCarBook.DataAccessLayer/Migrations/20231217182411_mig_pricestatus.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyAcademyCarBook.DataAccessLayer.Migrations
+{
+    public partial class mig_pricestatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "PriceValue",
+                table: "Prices",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PriceValue",
+                table: "Prices",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
